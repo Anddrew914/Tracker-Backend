76f417025c2242cc93f1b923a2d90c43f512494e
Andrew.Easterling <Anddrew914@gmail.com>
2017-06-15 17:50:25 -0400

    destroys old models, builds new ones.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
+  resources :comments
+  resources :posts
   resources :user_coins, except: [:new, :edit]
   resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
